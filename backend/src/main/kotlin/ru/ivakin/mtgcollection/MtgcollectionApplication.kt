01caa71bc1c9a7aad35fc8bb09c2fd2e4d42fe90
aivakin <Aleksandr.Ivakin@noveogroup.com>
2022-02-12 17:28:07 +0700

    simple database done

--- backend/src/main/kotlin/ru/ivakin/mtgcollection/MtgcollectionApplication.kt
+++ backend/src/main/kotlin/ru/ivakin/mtgcollection/MtgcollectionApplication.kt
@@ -6,6 +6,15 @@ import org.springframework.data.annotation.Id
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
+
+import org.springframework.stereotype.Service
+
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PostMapping
+
 @SpringBootApplication
 class MtgcollectionApplication
 
@@ -14,13 +23,30 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource {
+class MessageResource(val service: MessageService) {
     @GetMapping
-    fun index(): List<Message> = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "ZDAROVA"),
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
 }
 
-data class Message(val id: String?, val text: String)
+@Table("MESSAGE")
+data class Message(@Id val id: String?, val text: String)
+
+interface MessageRepository: CrudRepository<Message, String> {
+    @Query("SELECT * FROM message")
+    fun findMessages(): List<Message>
+
+}
+
+@Service
+class MessageService(val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findMessages()
+
+    fun post(message: Message) {
+        db.save(message)
+    }
+}

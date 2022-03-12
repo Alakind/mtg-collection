2e5b83143588f48f6c028233138b8e4bb1c78248
aivakin <Aleksandr.Ivakin@noveogroup.com>
2022-03-12 13:31:55 +0700

    frontend init

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'import/extensions': [
+      2,
+      {
+        tsx: 'never',
+      },
+    ],
+    'import/no-unresolved': 0,
+  },
+};
